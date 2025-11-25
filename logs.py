521dfeddf889d2621a1ef4e605f0b7714638e851
shrine <114279874+shr1ne@users.noreply.github.com>
2025-11-25 14:03:57 +0800

    updated logs.py
    will revert if problems arise

--- logs.py
+++ logs.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 import os
 from dotenv import load_dotenv
-
+import json
 
 
 try:
@@ -16,26 +16,23 @@ except Exception as e:
     print(f"Error Loading Credentials: {e}")
 
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# print(os.path.exists(os.path.join(current_dir, FIREBASE_CREDENTIALS)))
-
-
 class DocumentLogs:
 
     def __init__(self):
 
-        self.cred_path = os.path.join(current_dir, FIREBASE_CREDENTIALS)
-        self.cred = credentials.Certificate(self.cred_path)
-        #initialize firebase app if not already done
+        try:
+            cred_dict = json.loads(FIREBASE_CREDENTIALS)
+        except Exception as e:
+            raise ValueError(f"Invalid FIREBASE_CREDENTIALS JSON: {e}")
+
+        self.cred = credentials.Certificate(cred_dict)
+
+        # initialize firebase app if not already done
         if not firebase_admin._apps:
             firebase_admin.initialize_app(self.cred)
 
-        #now safely create the firestore client
+        # create firestore client
         self.db = firestore.client()
-
-
-
 
     def paradise_logs(self, input, valid, searches ):
         doc_ref = self.db.collection('love-in-paradise-logs').document()
@@ -49,30 +46,12 @@ class DocumentLogs:
         doc_ref.set(data)
         print("Successfully written:", doc_ref.id)
 
-
-    
     def user_input(self, user_input):
         return user_input
+
     def valid_claim(self, valid_claim):
         return valid_claim
+
     def search_log(self, search_log):
         print("testing")
         return search_log
-
-
-# logs = DocumentLogs()
-# print(logs.paradise_logs(logs.tokens()))
-
-
-# firebase_admin.initialize_app(cred)
-
-
-# db = firestore.client()
-# doc_ref = db.collection('love-in-paradise-logs').document()
-
-# data = {
-#     'logs': 'wash hands',
-#     'log id': doc_ref.id
-    
-# }
-# doc_ref.set(data)
